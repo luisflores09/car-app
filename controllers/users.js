3ed9e2d9906433f89fb16067d1817da2fbde087d
Luis <iluisflores@yahoo.com>
2021-10-11 08:45:06 -0500

    "Signup Page saves username & password to MongoDB"

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
 
 
 router.get('/login', (req, res) => {
     res.render('login.ejs', { error: ''});
+});
+
+
+router.get('/signup', (req, res) => {
+    res.render('signup.ejs');
+});
+
+
+router.post('/signup', (req, res) => {
+    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+    User.create(req.body, (err, user) => {
+        console.log(user);
+        res.redirect('/login');
+    });
 });
 
 module.exports = router;
